b46e4b06299e292b72e025637972f855c2da3b64
Victor Hamon <victor.hamo@epitech.eu>
2024-11-14 21:17:15 +0100

    refactor: add tests for exchange rate repository (00000)

--- app/api/data_sources/exchange_rate_repository.py
+++ app/api/data_sources/exchange_rate_repository.py
@@ -13,25 +13,43 @@ class ExchangeRateRepository:
     class UnexpectedResponseError(Exception):
         pass
 
-    def __init__(self, api_url: str, api_key: str):
+    class RequestHandlerError(Exception):
+        pass
+
+    class CurrencyNotFoundError(Exception):
+        pass
+
+    class TargetCurrencyNotFoundError(Exception):
+        pass
+
+    def __init__(self, api_url: str, api_key: str, request_handler=requests):
         self.api_key = api_key
         self.url = f"{api_url}{api_key}/latest/"
+        self.request_handler = request_handler
 
     def get_exchange_rate_for_country(self, to_currency: CountryCode, base_currency: CountryCode = "EUR") -> Dict[CountryCode, float]:
         try:
-            response = requests.get(f"{self.url}{base_currency}")
+            response = self.request_handler.get(f"{self.url}{base_currency}")
 
             if response.status_code == 403:
                 raise self.ForbiddenAccessError("Access to the API is denied")
+            if response.status_code >= 500:
+                raise self.UnexpectedResponseError("Unexpected response from API")
+            if response.status_code == 404:
+                raise self.CurrencyNotFoundError(f"Currency code {to_currency} not found")
 
             data = response.json()
             conversion_rate = data["conversion_rates"]
 
-            return {
-                base_currency: conversion_rate[base_currency],
-                to_currency: conversion_rate[to_currency],
-            }
+
+            try:
+                return {
+                    base_currency: conversion_rate[base_currency],
+                    to_currency: conversion_rate[to_currency],
+                }
+            except KeyError:
+                raise self.TargetCurrencyNotFoundError(f"Currency code {to_currency} not found")
 
         except requests.exceptions.RequestException as err:
-            raise self.UnexpectedResponseError(f"Unexpected response from API: {err}")
+            raise self.RequestHandlerError(err)
 
---
+++ app/tests/test_data_sources/test_exchange_rate_repository.py
@@ -0,0 +1,90 @@
+from unittest.mock import create_autospec
+
+import pytest
+import requests
+
+from app.api.data_sources.exchange_rate_repository import ExchangeRateRepository
+from app.api.types import CountryCode
+
+
+@pytest.fixture
+def fake_request_handler():
+    return create_autospec(requests)
+
+class TestExchangeRateRepository:
+
+    def test_should_raise_when_access_to_the_repository_is_forbidden(self, fake_request_handler):
+        fake_request_handler.get.return_value.status_code = 403
+
+        exchange_rate_repository = ExchangeRateRepository("", "", request_handler=fake_request_handler)
+
+        with pytest.raises(ExchangeRateRepository.ForbiddenAccessError):
+            exchange_rate_repository.get_exchange_rate_for_country(CountryCode("USD"))
+
+    def test_should_raise_when_unexpected_error_occurs(self, fake_request_handler):
+        fake_request_handler.get.return_value.status_code = 500
+
+        exchange_rate_repository = ExchangeRateRepository("", "", request_handler=fake_request_handler)
+
+        with pytest.raises(ExchangeRateRepository.UnexpectedResponseError):
+            exchange_rate_repository.get_exchange_rate_for_country(CountryCode("USD"))
+
+    def test_should_raise_when_request_handler_error_occurs(self, fake_request_handler):
+        fake_request_handler.get.side_effect = requests.exceptions.RequestException
+
+        exchange_rate_repository = ExchangeRateRepository("", "", request_handler=fake_request_handler)
+
+        with pytest.raises(ExchangeRateRepository.RequestHandlerError):
+            exchange_rate_repository.get_exchange_rate_for_country(CountryCode("USD"))
+
+    def test_should_return_exchange_rate_for_given_country_code_when_it_exists(self, fake_request_handler):
+
+        expected_rate = 2.0
+        currency_to_convert = CountryCode("AUD")
+        base_currency = CountryCode("USD")
+
+        fake_request_handler.get.return_value.json.return_value = {
+            "result": "success",
+            "conversion_rates": {
+                base_currency: 1,
+                currency_to_convert: expected_rate
+            }
+        }
+        fake_request_handler.get.return_value.status_code = 200
+
+
+        exchange_rate_repository = ExchangeRateRepository("", "", request_handler=fake_request_handler)
+        conversion_rates = exchange_rate_repository.get_exchange_rate_for_country(currency_to_convert, base_currency)
+
+        assert conversion_rates == {
+            base_currency: 1,
+            currency_to_convert: expected_rate
+        }
+
+    def test_should_raise_when_base_country_code_does_not_exist_in_conversion_rates(self, fake_request_handler):
+        base_currency = CountryCode("USD")
+
+        fake_request_handler.get.return_value.status_code = 404
+
+        exchange_rate_repository = ExchangeRateRepository("", "", request_handler=fake_request_handler)
+
+        with pytest.raises(ExchangeRateRepository.CurrencyNotFoundError):
+            exchange_rate_repository.get_exchange_rate_for_country(..., base_currency)
+
+
+    def test_should_raise_when_target_currency_does_not_have_conversion_rate_for_given_base_currency(self, fake_request_handler):
+        base_currency = CountryCode("USD")
+        target_currency_to_convert = CountryCode("AUD")
+
+        fake_request_handler.get.return_value.json.return_value = {
+            "result": "success",
+            "conversion_rates": {
+                base_currency: 1,
+            }
+        }
+        fake_request_handler.get.return_value.status_code = 200
+
+        exchange_rate_repository = ExchangeRateRepository("", "", request_handler=fake_request_handler)
+
+        with pytest.raises(ExchangeRateRepository.TargetCurrencyNotFoundError):
+            exchange_rate_repository.get_exchange_rate_for_country(target_currency_to_convert, base_currency)
