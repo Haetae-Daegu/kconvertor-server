6c6e886066e2882da7ef71b31059be1cf0bb7ca7
NicolasYapobi <nicolas.yapobi@epitech.eu>
2024-12-03 22:12:31 +0100

    Update: HTTP call in currency route Swagger definition

--- app/api/api.py
+++ app/api/api.py
@@ -1,20 +1,23 @@
 import requests, os
-from app.api import FROM_CURRENCY, TO_CURRENCY, API_URL
+from app.api import API_URL
 from app.error import APIError
-from flask import Blueprint
+from flask import Blueprint, request
 
 api_bp = Blueprint("api_bp", __name__)
 
 
-@api_bp.route("/currency/")
+@api_bp.route("/currency/", methods=['POST'])
 def json_currency():
-    country = FROM_CURRENCY
-    return exchange_rate(country)
+    print(request.json)    
+    from_currency = request.json["from_currency"]
+    to_currency = request.json["to_currency"]
+    amount = request.json["amount"]
+    return exchange_rate(from_currency, to_currency, amount)
 
 
-def exchange_rate(country="EUR"):
+def exchange_rate(from_currency: str = "EUR", to_currency:str = "KRW", amount:float = 1):
     api_key = os.environ.get("EXCHANGE_RATE_API_KEY")
-    url = f"{API_URL}{api_key}/latest/{country}"
+    url = f"{API_URL}{api_key}/pair/{from_currency}/{to_currency}/{amount}"
 
     try:
         response = requests.get(url)
@@ -22,16 +25,18 @@ def exchange_rate(country="EUR"):
             raise APIError(403, "Forbidden: Access to the API is denied")
 
         data = response.json()
-        conversion_rate = data["conversion_rates"]
+        print(data)
 
-        return get_conversion_rate(conversion_rate)
+        return get_conversion_rate(data, amount)
 
     except requests.exceptions.RequestException as err:
         raise APIError(500, f"Connection Error: {err}")
+    except Exception as e:
+        raise APIError(500, f"Unexpected Exception: {e} is incorrect or missing")
 
 
-def get_conversion_rate(conversion_rate):
+def get_conversion_rate(data, amount: float):
     return {
-        FROM_CURRENCY: conversion_rate[FROM_CURRENCY],
-        TO_CURRENCY: conversion_rate[TO_CURRENCY],
+        data["base_code"]: amount,
+        data["target_code"]: round(data["conversion_result"], 2),
     }
