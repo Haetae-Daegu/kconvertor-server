3a5f073f45a4d5c49f85dc21ac720b47c90639f5
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-01-27 17:51:50 +0100

    feat - graph: update script

--- app/data_script/scrap_history.py
+++ app/data_script/scrap_history.py
@@ -2,23 +2,24 @@ from bs4 import BeautifulSoup
 import requests
 
 
-
-
 def get_data(soup):
     data = []
     table = soup.find(class_="history-rates-data")
     rows = table.find_all("td")
-    for line in rows:
-        if line.find("a"):
-            date = line.text.strip().split("\n")[1]
-        if line.find("span"):
-            currency_value = line.text.strip().split("\n")[0]
-            to_remove = [("1 EUR = ", ""), ("KRW", ""), ("\xa0", "")]
-            [currency_value := currency_value.replace(a, b) for a, b, in to_remove]
-            obj = {"date": date, "currency_value": currency_value}
-            data.append(obj)
-    data.pop()
-    print(data)
+    with open("../../data/historical_currency.txt", "w", encoding="utf-8") as file:
+        for line in rows:
+            if line.find("a"):
+                date = line.text.strip().split("\n")[1]
+            if line.find("span"):
+                currency_value = line.text.strip().split("\n")[0]
+                to_remove = [("1 EUR = ", ""), ("KRW", ""), ("\xa0", "")]
+                [currency_value := currency_value.replace(a, b) for a, b, in to_remove]
+                
+                if "Taux de change" in currency_value:
+                    continue
+
+                formatted_line = f"KRW\t{date}\t{currency_value}\n"
+                file.write(formatted_line)
 
 def get_page(url):
     headers = {
@@ -33,9 +34,11 @@ def get_page(url):
 
 def scrap():
     page_content = get_page("https://www.exchange-rates.org/fr/historique/eur-krw-2025")
-    soup = BeautifulSoup(page_content, "html.parser")
-    get_data(soup)
-
+    if page_content:
+        soup = BeautifulSoup(page_content, "html.parser")
+        get_data(soup)
+    else:
+        print("Error connection or URL")
     
 if __name__ == "__main__":
     scrap()
