b638ce578bd2251c68caceca69170ac733381352
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-03-22 18:53:23 +0100

    refactor: remove unused import from auth.py to clean up code

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, verify_jwt_in_request
+from flask_jwt_extended import create_access_token, get_jwt_identity, verify_jwt_in_request
 from app.services.user_service import *
 from app.services.auth_service import *
 from pydantic import BaseModel, ValidationError
